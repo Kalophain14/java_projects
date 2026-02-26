f0382aab859048ba205ecc908b6755d84cd80055
‘Kalophain14’ <chueutemosho@gmail.com>
2026-02-26 22:15:09 +0200

    added color, displays, buttons

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -5,8 +5,8 @@ import javax.swing.*;
 import javax.swing.border.LineBorder; //modify boarders on the buttons
 
 public class Calculator {
+    
     // Window Frame
-
     int boardWidth =  360;
     int boardHeight = 540;
 
@@ -14,9 +14,13 @@ public class Calculator {
     Color customDarkGrey = new Color(80, 80, 80);
     Color customBlack = new Color(28, 28, 28);
     Color customOrange = new Color(255,149, 0);
-    
 
+    
+    //Java Swing components
     JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel(); //display the numbers on the screen, right aligned
+    JPanel displayPanel = new JPanel(); //display the buttons
+
 
     //Construction
     Calculator (){
@@ -26,5 +30,17 @@ public class Calculator {
         frame.setResizable(false); //user doesn't drag the sides outside of the window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //x button to close the app
         frame.setLayout(new BorderLayout()); //north, south, west and east within the window
+
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT); //align the numbers to the right
+        displayLabel.setText(0);
+        displayLabel.setOpaque(true); //make the background color visible
+        
+        //display panel
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel);
     }
 }
