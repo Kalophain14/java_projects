8a036f75408762996ce113306d1e0729d89c3cec
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-06 21:47:28 +0200

    Remove IDE and build files from tracking

---
+++ bankingProgram/src/bankingApp.java
@@ -0,0 +1,10 @@
+// ============================================================
+//  BankingApp.java  —  Run this file to start the program!
+// ============================================================
+
+public class bankingApp {
+    public static void main(String[] args) {
+        bankingProgram program = new bankingProgram();
+        program.run();
+    }
+}
