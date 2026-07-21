5ddebd828029b3749d35491434306ff966382c76
‘Kalophain14’ <chueutemosho@gmail.com>
2026-07-21 19:34:16 +0200

    initalizing the main file, by packaging, importing and enabling audits, annotations, tracking

--- 03_bankcore-api/src/main/java/com/bankcore/api/BankCoreApiApplication.java
+++ 03_bankcore-api/src/main/java/com/bankcore/api/BankCoreApiApplication.java
@@ -1,3 +1,17 @@
 package com.bankcore.api;
 
-// TODO: Implement this class
+package com.bankcore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class BankCoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankCoreApiApplication.class, args);
+    }
+}
+
