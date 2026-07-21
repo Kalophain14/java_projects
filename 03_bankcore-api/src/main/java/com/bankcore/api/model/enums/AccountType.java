517e5fe1066663a100c11f9d8e9fda82b565ce73
‘Kalophain14’ <chueutemosho@gmail.com>
2026-07-21 19:53:32 +0200

    creating the list for the banking api, using enum this helps creates classes that has objects for each account on the enum

--- 03_bankcore-api/src/main/java/com/bankcore/api/model/enums/AccountType.java
+++ 03_bankcore-api/src/main/java/com/bankcore/api/model/enums/AccountType.java
@@ -1,3 +1,17 @@
 package com.bankcore.api.model.enums;
 
-// TODO: Implement this class
+/*
+Enums are like a list of accounts available on the api,
+that can be read by the Bank core API
+
+- It helps you to catch typos,
+- Won't compile if the item isn't on the list
+- Java is able to pick its meaning
+- You can only pick a valid option, otherwise won't run
+- When ENUM is called Java will creates CLASS with an OBJECT for each item on the list
+ */
+public enum AccountType {
+    SAVINGS,
+    CHEQUE,
+    MINOR
+}
